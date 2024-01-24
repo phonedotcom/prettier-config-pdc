f7f5f8a46f5cdd17dff41fb755ca81eb904c5c41
Jacek Bajor <jbajor@gmail.com>
2024-01-24 11:27:37 +0100

    Update to  @prettier/plugin-xml

--- prettier.config.js
+++ prettier.config.js
@@ -2,7 +2,11 @@ export default {
   semi: false,
   singleQuote: true,
   trailingComma: 'none',
-  plugins: ['prettier-plugin-embed', 'prettier-plugin-sql', 'prettier-plugin-xml'],
+  plugins: [
+    'prettier-plugin-embed',
+    'prettier-plugin-sql',
+    '@prettier/plugin-xml'
+  ],
   // prettier-plugin-sql options
   formatter: 'sql-formatter',
   keywordCase: 'upper',
